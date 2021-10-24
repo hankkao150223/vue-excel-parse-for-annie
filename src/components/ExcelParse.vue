93fd0b772111a7be6a99a70118da81eac854daed
Hank-Nash <hankkaonash@dentall.io>
2021-10-24 19:06:03 +0800

    feat: change excel export name

--- src/components/ExcelParse.vue
+++ src/components/ExcelParse.vue
@@ -115,9 +115,9 @@ export default {
       /* convert state to workbook */
       const ws = XLSX.utils.aoa_to_sheet(this.data);
       const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, 'SheetJS');
+      XLSX.utils.book_append_sheet(wb, ws, 'sheet');
       /* generate file and send to client */
-      XLSX.writeFile(wb, 'sheetjs.xlsx');
+      XLSX.writeFile(wb, 'sheet.xlsx');
     },
     _file(file) {
       /* Boilerplate to set up FileReader */
